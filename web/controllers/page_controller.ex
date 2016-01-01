0107d5b3949c8fa9c29feaa56177f158711830bc
Kyle Scully <scullykns@gmail.com>
2016-01-01 10:14:05 -0800

    controllers - flash notice

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,20 @@ defmodule HelloPhoenix.PageController do
   use HelloPhoenix.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    # render conn, "index.html"
+
+    conn
+    |> put_flash(:info, "Welcome to Phoenix, from flash info!")
+    |> put_flash(:error, "Let's pretend we have an error.")
+    |> render("index.html")
   end
 end
+
+# review of RESTful
+# index - renders a list of all items of the given resource type
+# show - renders an individual item by id
+# new - renders a form for creating a new item
+# create - receives params for one new item and saves it in a datastore
+# edit - retrieves an individual item by id and displays it in a form for editing
+# update - receives params for one edited item and saves it to a datastore
+# delete - receives an id for an item to be deleted and deletes it from a datastore
